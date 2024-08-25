1277e7d3dab3170df5e675f5303c3bd932b5cae0
ThangB <buiducthang652@gmail.com>
2024-08-25 07:38:28 +0700

    init

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const fs = require("fs");
 
-server.use(middlewares);
-server.use(router);
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log("JSON Server is running");
+// Function to load data from JSON files
+function loadData() {
+  let data = {};
+  const files = [
+    "baotran.json",
+    "hainam.json",
+    "minhduc.json",
+    "tranvan.json",
+    "tuanthanh.json",
+  ];
+  files.forEach((file) => {
+    try {
+      let content = JSON.parse(fs.readFileSync(file, "utf8"));
+      // Combine all files into one object, make sure the keys don't overlap
+      Object.keys(content).forEach((key) => {
+        if (data[key] === undefined) {
+          data[key] = [];
+        }
+        data[key] = data[key].concat(content[key]);
+      });
+    } catch (error) {
+      console.error(`Error loading or parsing file ${file}: ${error}`);
+    }
+  });
+  return data;
+}
+
+const router = jsonServer.router(loadData()); // Use the loadData function to provide the data for the router
+
+// Rewrite rules for the base path
+const rules = jsonServer.rewriter({
+  "/api/baotran/*": "/baotran/$1",
+  "/api/hainam/*": "/hainam/$1",
+  "/api/minhduc/*": "/minhduc/$1",
+  "/api/tranvan/*": "/tranvan/$1",
+  "/api/tuanthanh/*": "/tuanthanh/$1",
+});
+
+server.use(middlewares); // Use default middlewares (logger, static, cors and no-cache)
+server.use(rules); // Apply rewrite rules
+server.use(router); // Use the router
+
+server.listen(3000, () => {
+  console.log("JSON Server is running on port 3000");
 });
